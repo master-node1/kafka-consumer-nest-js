0579605d872a0a00b573c80cc36562cf58a71760
Sateesh Raja Sireddi <hari.sireddi7776@gmail.com>
2025-07-30 16:36:35 +0530

    Delete dist directory

--- dist/controller/app.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Logger } from '@nestjs/common';
-export declare class AppController {
-    private readonly logger;
-    constructor(logger: Logger);
-    handleTopic1Message(data: any): Promise<any>;
-    handleTopic2Message(data: any): Promise<any>;
-}
--- dist/trace.d.ts
+++
@@ -1,8 +0,0 @@
-declare class Tracer {
-    private sdk;
-    private exporter;
-    private provider;
-    init(): void;
-}
-declare const _default: Tracer;
-export default _default;
